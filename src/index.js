0d51bfa07c127ed3d7cd2905264aedd595d3f590
AevumElectronics <aevumelectronics@gmail.com>
2022-04-30 21:00:54 +0200

    test branch 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import {BrowserRouter, Route, Redirect} from 'react-router-dom'
-import * as ReactDOMClient from 'react-dom/client';
 import './assets/main.css';
 import App from './App';
 import './assets/tailwind.css';
